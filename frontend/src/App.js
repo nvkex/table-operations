26cb1234f24a013ae915e67d8c054ea4b37bac11
nvkex <scrbwrm@gmail.com>
2020-09-21 22:12:39 +0530

    landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Stage1 from './components/Stage1';
 
-function App() {
-  return (
-    <div className="App">
-      Data Analyser
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    stage: 1
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+
+        <div className="stage-container">
+          <Stage1 />
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
