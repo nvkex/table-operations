b819589bbd2dd9bc13b3b068211528d12b937979
nvkex <scrbwrm@gmail.com>
2020-09-21 11:31:19 +0530

    get fields and join two collections

--- controllers/db.js
+++ controllers/db.js
@@ -1,19 +1,20 @@
 const mongoose = require('mongoose');
+const { joinTables } = require('./table');
 
-exports.collectionList = async(req, res) => {
+exports.collectionList = async (req, res) => {
 
-    mongoose.connection.db.listCollections().toArray( (err, data) => {
+    mongoose.connection.db.listCollections().toArray((err, data) => {
         const names = [...data.map(name => name.name)]
         console.log(names);
-        res.send({names});
+        res.send({ names });
     });
 
 }
 
-exports.listFields = (req,res) => {
+exports.fieldList = (req, res) => {
     const collection = req.body.coll;
 
-    const tempSchema = new mongoose.Schema({},{
+    const tempSchema = new mongoose.Schema({}, {
         strict: false,
         strictQuery: false
     });
@@ -21,16 +22,55 @@ exports.listFields = (req,res) => {
     const tempModel = mongoose.model(collection, tempSchema);
 
     tempModel.findOne({})
-    .then(data => {
-        const fields = Object.keys(data._doc)
-        res.send(fields);
-    })
-    .catch(err => {
-        console.log(err);
-        res.send({error: err});
-    })
+        .then(data => {
+            const fields = Object.keys(data._doc)
+            res.send(fields);
+        })
+        .catch(err => {
+            console.log(err);
+            res.send({ error: err });
+        })
 }
 
-exports.joinCollections = (req, res) => {
+exports.joinCollections = async (req, res) => {
+    const collection1 = req.body.coll1;
+    const collection2 = req.body.coll2;
+    const column = req.body.column;
+
+    // Temporary schemas
+    const tempSchema1 = new mongoose.Schema({}, {
+        strict: false,
+        strictQuery: false
+    });
+    const tempSchema2 = new mongoose.Schema({}, {
+        strict: false,
+        strictQuery: false
+    });
+
+    // Models
+    const tempModel1 = mongoose.model(collection1, tempSchema1);
+    const tempModel2 = mongoose.model(collection2, tempSchema2);
+
+    var coll1Data;
+    var coll2Data;
+
+    await tempModel1.find({}, { _id: 0 })
+        .then(data => {
+            coll1Data = [
+                ...
+                data.map(entry => JSON.parse(JSON.stringify(entry)))
+            ];
+        }).catch(err => { res.send({ error: err }) });
+
+    await tempModel2.find({}, { _id: 0 })
+        .then(data => {
+            coll2Data = [
+                ...
+                data.map(entry => JSON.parse(JSON.stringify(entry)))
+            ];
+        }).catch(err => { res.send({ error: err }) });
+
+    const DF = joinTables(coll1Data, coll2Data, column);
+    res.send({ csv: DF.to_csv() });
 
 }
