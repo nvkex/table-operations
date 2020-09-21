005e6d58db61f709371ed3abe53560dd4aa826b2
nvkex <scrbwrm@gmail.com>
2020-09-21 22:13:03 +0530

    navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+        <span className="circle"></span>
+        <span>
+            Table Operations
+        </span>
+    </div>
+  )
+}
+
+export default Navbar
