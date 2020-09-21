4f06483facd956453a3644ab631c006dc85cabf4
nvkex <scrbwrm@gmail.com>
2020-09-21 11:30:56 +0530

    export for reuses

--- controllers/table.js
+++ controllers/table.js
@@ -3,27 +3,29 @@ const { DataFrame } = require('pandas-js');
 const joinTables = (arr1, arr2, col) => {
     const df1 = new DataFrame(arr1);
     const df2 = new DataFrame(arr2);
-    const newDf = df1.merge(df2,[col],'inner');
+    const newDf = df1.merge(df2, [col], 'inner');
     return newDf;
 
 }
 
 const sortByField = (req, res) => {
-    res.send({msg: 'Hello'})
+    res.send({ msg: 'Hello' })
 }
 
-exports.configTables = (req,res) => {
+const configTables = (req, res) => {
     const { join, sort } = req.query;
     var DF;
-    
+
     // Join Tables
-    if(join){
+    if (join) {
         DF = joinTables(req.body.df1, req.body.df2, req.body.col);
     }
-    if(sort){
+    if (sort) {
 
     }
 
-    res.send({csv: DF.to_csv()});
-    
+    res.send({ csv: DF.to_csv() });
+
 }
+
+module.exports = { joinTables, configTables }
