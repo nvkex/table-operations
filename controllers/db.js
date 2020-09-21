abae93a916211949cf37933dcc70c1e9ccd815aa
nvkex <scrbwrm@gmail.com>
2020-09-21 10:29:30 +0530

    get list of fields in a collection

--- controllers/db.js
+++ controllers/db.js
@@ -1,18 +1,31 @@
 const mongoose = require('mongoose');
 
 exports.collectionList = async(req, res) => {
-    const URL = req.body.dbURL;
-
-    await mongoose.connect(URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then( response => {console.log('Connected to DB')})
-    .catch( err => {console.log(e)});
 
     mongoose.connection.db.listCollections().toArray( (err, data) => {
         const names = [...data.map(name => name.name)]
         console.log(names);
         res.send({names});
+    });
+
+}
+
+exports.joinCollections = async(req,res) => {
+    const collection1 = req.body.coll1;
+
+    const tempSchema = new mongoose.Schema({},{
+        strict: false,
+        strictQuery: false
+    });
+
+    const tempModel = mongoose.model(collection1, tempSchema);
+
+    tempModel.findOne({})
+    .then(data => {
+        console.log(data);
+        res.send(Object.keys(data._doc));
+    })
+    .catch(err => {
+        console.log(err)
     })
 }
